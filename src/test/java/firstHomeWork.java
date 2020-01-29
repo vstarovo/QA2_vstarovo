f949ab17fc5fae149d21d9408e3f6fd1256f50cf
vstarovo <vstarovoitovs@gmail.com>
2020-01-29 13:22:58 +0200

    chaged names of values and added round function

--- src/test/java/firstHomeWork.java
+++ src/test/java/firstHomeWork.java
@@ -4,65 +4,51 @@ import java.util.Arrays;
 
 public class firstHomeWork {
 
-    // 1 zadanie
+    // 1
     @Test
     public void personData() {
         showNameAge("Дима, ", 33);
     }
 
-    private void showNameAge(String a, int b) {
-        System.out.println("Привет " + a + "тебе уже " + b);
+    private void showNameAge(String name, int age) {
+        System.out.println("Привет " + name + "тебе уже " + age);
     }
 
-    // 2 zadanie
-
-    private double fuelConsumption(double l, double s) {
-        return (l / (s / 100));
+    // 2
+    private double fuelConsumption(double fuelcount, double distance) {
+        return (fuelcount / (distance / 100));
     }
 
     @Test
     public void showFuelConsumption() {
-        double x;
-        x = fuelConsumption(35, 500);
-        System.out.println("Литров " + x + "на 100км");
+        double consumption;
+        consumption = fuelConsumption(37, 505);
+        System.out.println("Расход топлива =  " + Math.round(consumption * 100) / 100.0d + " Л/100км");
     }
 
-   /*public void countOfRounds() {
-   double r = 0.8;
-   double z = 1000000/(2*3.14*r);
-   System.out.println("Кол-во оборотов " + z + " на 1000000км");}*/
-
-    // 3 zadanie
-
-    private double countOfRounds(double r) {
-        return 1000000 / (2 * 3.14 * r);
-        //return Math.round((1000000 / (2 * 3.14 * r)),2);
+    // 3
+    private double countOfRounds(double radius) {
+        return 1000000 / (2 * 3.14 * radius);
     }
 
     @Test
     public void showcountOfRounds() {
-        double z;
-        z = countOfRounds(0.8);
-        System.out.println("Кол-во " + z + " на 1000000км");
+        double radius;
+        radius = countOfRounds(0.8);
+        System.out.println("Кол-во оборотов " + Math.round(radius * 100) / 100.0d + " на 1000000км");
     }
 
-
-//  4 zadanie
-
+    //  4
     private double distanceTwoPoints(double x1, double x2, double y1, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
-
     @Test
     public void showDistanceTwoPoints() {
-        double d;
-        d = distanceTwoPoints(1, 3, 1, 3);
-        System.out.println("Растояние между двуми точками =  " + d);
+        double distance;
+        distance = distanceTwoPoints(1, 3, 1, 3);
+        System.out.println("Растояние между двуми точками =  " + Math.round(distance * 100) / 100.0d);
     }
-
-
-
 
 }
 
